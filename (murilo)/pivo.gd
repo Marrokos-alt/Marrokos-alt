0701dff72200efeede053f63686145e28dbe1589
MUriLO1912 <salatinemurilo@gmail.com>
2026-08-22 14:04:56 -0300

    segundo commit
    -Correção da camera e movimentação do player
    -Inimigo adicionado
    -Ataque de espada adicionado ao player
    -Invencibilidade no player apos tomar um dano
    -Knockback adicionado

--- (murilo)/pivo.gd
+++ (murilo)/pivo.gd
@@ -1,13 +1,1 @@
 extends SpringArm3D
-
-@export var sensibilidade: float = 0.005
-
-func _ready() -> void:
-	self.top_level = false
-
-func _unhandled_input(event: InputEvent) -> void:
-	if event is InputEventMouseMotion:
-		rotation.y -= event.relative.x * sensibilidade
-		rotation.y = wrapf(rotation.y, 0.0, TAU)
-		rotation.x -= event.relative.y * sensibilidade
-		rotation.x = clamp(rotation.x, -PI/2, PI/4)
--- (murilo)/player3.gd
+++ (murilo)/player3.gd
@@ -1,14 +1,28 @@
 extends CharacterBody3D
 
+@export var sensibilidade: float = 0.005
 @export var SPEED = 5.0
 const JUMP_VELOCITY = 4.5
 var nascer:  bool = true
+var Vida: float = 100
+var knockback: Vector3
+var forcaK: float = 5.0
+var atrito: float = 20.0
+var inimigos = []
 
 func _ready() -> void:
 	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
 	$Timer.start()
 
+func _unhandled_input(event: InputEvent) -> void:
+	if event is InputEventMouseMotion:
+		rotation.y -= event.relative.x * sensibilidade
+		$Pivo.rotation.x -= event.relative.y * sensibilidade
+		$Pivo.rotation.x = clamp($Pivo.rotation.x, deg_to_rad(-90), deg_to_rad(45))
+
 func _process(delta: float) -> void:
+	$CanvasLayer/ProgressBar.value = Vida
+	
 	if nascer:
 		$CanvasLayer/MeshInstance2D.self_modulate.a -= delta
 		if $CanvasLayer/MeshInstance2D.self_modulate.a <= 0.0:
@@ -25,12 +39,19 @@ func _process(delta: float) -> void:
 	
 	if Input.is_action_pressed("zoom_n"):
 		$Pivo.spring_length = 6.0
+	
+	if Input.is_action_just_pressed("M1"):
+		if $Area3D/Atk.is_stopped():
+			$Area3D/Atk.start()
+			$Area3D/MeshInstance3D.show()
+			$Area3D/sumir.start()
+			inimigos = inimigos.filter(func(i): return is_instance_valid(i))
+			for i: Node3D in inimigos:
+				if not i.is_in_group("Player"):
+					if i.has_method("LevarDano"):
+						i.LevarDano(1, global_position)
 
 func _physics_process(delta: float) -> void:
-	var d = abs(wrapf(rotation.y - $Pivo.rotation.y, -PI, PI))
-	if d <= 1.0:
-		rotation.y = lerp(rotation.y, $Pivo.rotation.y, 0.1)
-	
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 	
@@ -38,9 +59,12 @@ func _physics_process(delta: float) -> void:
 		velocity.y = JUMP_VELOCITY
 	
 	if Input.is_action_just_pressed("Correr"):
-		SPEED = 7.0
+		SPEED = 8.0
 	else:
 		SPEED = 5.0
+	
+	if knockback != Vector3.ZERO:
+		knockback = knockback.move_toward(Vector3.ZERO, atrito * delta)
 	
 	var input_dir := Input.get_vector("left", "right", "up", "down")
 	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
@@ -51,4 +75,48 @@ func _physics_process(delta: float) -> void:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		velocity.z = move_toward(velocity.z, 0, SPEED)
 	
+	velocity.x += knockback.x
+	velocity.z += knockback.z
+	velocity.y += knockback.y
+	knockback.y = 0.0
+	
 	move_and_slide()
+
+func LevarDano(dano: float = 0.0, pi: Vector3 = Vector3.ZERO):
+	if $Invencivel.is_stopped():
+		Vida -= dano
+		var dir = global_position.direction_to(pi) * -1
+		var h = Vector3(dir.x, 0, dir.z)
+		knockback = h * forcaK + Vector3.UP * 2.0
+		print(dano)
+		$Invencivel.start()
+		$Piscar.start()
+		if Vida <= 0:
+			print("Morto")
+
+func _on_invencivel_timeout() -> void:
+	$Piscar.stop()
+	$Invencivel.stop()
+	$MeshInstance3D.transparency = 0.0
+
+func _on_piscar_timeout() -> void:
+	if $MeshInstance3D.transparency == 0.0:
+		$MeshInstance3D.transparency = 0.6
+	else:
+		$MeshInstance3D.transparency = 0.0
+
+func _on_atk_timeout() -> void:
+	$Area3D/Atk.stop()
+
+func _on_area_3d_body_entered(body: Node3D) -> void:
+	if body.has_method("LevarDano"):
+		inimigos.append(body)
+
+func _on_area_3d_body_exited(body: Node3D) -> void:
+	if body.has_method("LevarDano"):
+		if body in inimigos:
+			inimigos.erase(body)
+
+func _on_sumir_timeout() -> void:
+	$Area3D/sumir.stop()
+	$Area3D/MeshInstance3D.hide()
